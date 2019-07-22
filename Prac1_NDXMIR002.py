a89ee76ace2eed3b0923ce95315c3d4f77d66bde
MirissaNaidu <naidu.missy@gmail.com>
2019-07-22 23:56:24 +0200

    Prac1_NDXMIR002.py

--- Prac1_NDXMIR002.py
+++ Prac1_NDXMIR002.py
@@ -1,6 +1,4 @@
-Python Practical Template
-Keegan Crankshaw
-Readjust this Docstring as follows:
+"""
 Names: Mirissa Naidu
 Student Number: NDXMIR002
 Prac: 1
@@ -13,25 +11,58 @@ import time
 from time import sleep
 
 # Define Variables
-    global led = 0
-    global bit = 0
-    global current_number = ""
+global LED_0 = 7             # LED PIN CONFIGURATIONS
+global LED_1 = 11
+global LED_2 = 15
+
+global button_increase = 16                     # BUTTON PIN CONFIGURATIONS
+global button_decrease = 18
+
+global led_on = 0             # decimal number
+global bit = 0                # bit displayed
+global current_number = ""    # binary 
+
+# Pin Configurations
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)                                         # setup pins using board config
+
+GPIO.setup(LED_0, GPIO.OUT, initial=GPIO.LOW)                    # sets initial condition to output low
+GPIO.setup(LED_1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_2, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(button_increase, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button using pull-up resistor
+GPIO.setup(button_decrease, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button using pull-up resistor
 
 # Logic that you write
+def add_bit():
+    while GPIO.input(button_increase) == GPIO.LOW:
+          sleep(0.01) 
+
+
+
+def minus_bit():
+    while GPIO.input(button_decrease) == GPIO.HIGH
+
+
+
+def counter():
+
+
 def main():
-    print("write your logic here")
-    
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
    
     while True: 
-          GPIO.output(8, GPIO.HIGH)
-          sleep(1)
-          GPIO.output(8, GPIO.LOW)
-          sleep(1)
-   
 
+	if add_bit(): 
+"""
+          GPIO.output(LED_0, GPIO.HIGH)
+          sleep(1)
+          GPIO.output(LED_0, GPIO.LOW)
+          sleep(1)
+"""
+          if GPIO.input(button_increase):
+              return False
+          else:
+  	      return True 
 
 # Only run the functions if
 if __name__ == "__main__":

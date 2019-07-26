c05c043b553a6bb0336fcab235747b49de134929
MirissaNaidu <naidu.missy@gmail.com>
2019-07-26 22:33:03 +0200

    Switched LED_0 on and off by pressing respective buttons

--- Prac1_NDXMIR002.py
+++ Prac1_NDXMIR002.py
@@ -1,3 +1,4 @@
+
 """
 Names: Mirissa Naidu
 Student Number: NDXMIR002
@@ -17,90 +18,45 @@ L1 = None                # bit displayed
 L2 = None                # MSB bit displayed
 SW0 = None
 SW1 = None
-binary = None            # shows the integer number displayed on LED 
+binary = None            # shows the integer number to be displayed on LED 
 current_number = 0
 
-# Pin set-up
+# GPIO configuration/Pin set-up
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)                                         # setup pins using board config
 
-GPIO.setup(7, GPIO.OUT, initial = GPIO.LOW)                    # sets initial condition to output low
+GPIO.setup(7, GPIO.OUT, initial = GPIO.LOW)                    # sets LED to output with initial value output low
 GPIO.setup(11, GPIO.OUT,initial = GPIO.LOW)
 GPIO.setup(15, GPIO.OUT,initial = GPIO.LOW)
 
-GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button using pull-down resistor
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button using pull-down resistor
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button on pin 16 using pull-down resistor
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)  # setup button on pin 18 using pull-down resistor
 
 
-def setLED(L2, L1, L0):
-	
-	if (L0 == 0):
-		GPIO.output(7, GPIO.LOW)
-        else: 
-                GPIO.output(7, GPIO.HIGH)
-       
-        if (L1 == 0):
-                GPIO.output(11, GPIO.LOW)
-        else:
-                GPIO.output(11, GPIO.HIGH)
 
-        if (L2 == 0):
-                GPIO.output(15, GPIO.LOW)
-        else:
-		GPIO.output(15, GPIO.HIGH)
 
-def displayLED(binary):
-	
-	if (binary == 0):
-		   setLED(0,0,0)
 
-        if (binary == 1):
-                   setLED(0,0,1)
-        
-	if (binary == 2):
-                   setLED(0,1,0)
-        
- 	if (binary == 3):
-                   setLED(0,1,1)
-        
-	if (binary == 4):
-                   setLED(1,0,0)
-        
-	if (binary == 5):
-                   setLED(1,0,1)
-        
-	if (binary == 6):
-                   setLED(1,1,0)
-        
-	if (binary == 7):
-                   setLED(1,1,1)
 
-       
+
+
 
 def main():
-   
-     input_state_1 = GPIO.input(18)
-     input_state_2 = GPIO.input(16)    
-     global current_number 
-         
-     if input_state_1 == False:
-        current_number += 1
 
-        if current_number>7:
-           current_number = 0
-        
-        displayLED(current_number)
-        sleep(0.2)
- 
-     if input_state_2 == False: 
-         print ('Button Pressed')
-         current_number = current_number - 1
- 
-         if current_number < 0:
-            current_number = 7
-  
-         displayLED(current_number)
-         sleep(0.2)
+	if GPIO.input(18) == False:
+
+		GPIO.output(7, GPIO.HIGH)
+		sleep(0.2)
+
+
+	if GPIO.input(16) == False:
+
+		GPIO.output(7, GPIO.LOW)
+		sleep(0.2)
+
+
+
+
+
 
 
 
